7f696c8f78f0eb4a28941db6b5f5c68f9b389844
mohamedGhodbane2003 <mohamedghodbane03@gmail.com>
2024-03-16 10:37:16 -0400

    bench flint

--- PLUQ/src/main_bench.c
+++ PLUQ/src/main_bench.c
@@ -1,36 +1,99 @@
 #include <stdio.h>
 #include <time.h>
 #include "utilities.h"
+#include <flint/fq_mat.h>
+#include <flint/fq.h>
+#include <flint/fmpz.h>
+#include <flint/flint.h>
+
 
 int main(int argc, char *argv[]) {
-    // list of primes, primes[k] has bitlength k+2
     int primes[29] = {3, 7, 13, 29, 53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469, 12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457};
+    
+    if (argc < 3 || (argv[1][0] != 'o' && argv[1][0] != 'f')) {
+        printf("Usage: ./bin/bench <o/f> size\n");
+        printf("  o: Benchmark AVX2 implementation against original implementation\n");
+        printf("  f: Benchmark AVX2 implementation against FLINT implementation\n");
+        return 1;
+    }
 
-    if (argc != 4) {
-        printf("BITS\t\t\tORIGINAL PLUQ\t\t\tVECTORIZED PLUQ\t\t\tSPEEDUP\n\n");
-        for (long k = 0; k < 29; k++) {
-            double tt = 0.0;
-            long nb_iter = 0;
-            while (tt < 1.) {
-                Matrix A = randomMatrix(200, 200, primes[k]);
-                int* P = NULL;
-                int* Q = NULL;
-                int rank = 0;
-                clock_t start = clock();
-                pluq_inplace(&A, &P, &Q, &rank, primes[k]);
-                clock_t end = clock();
-                tt += ((double)(end - start)) / CLOCKS_PER_SEC;
-                
-                nb_iter += 1;
+    if (argc == 3 && (argv[1][0] == 'o' || argv[1][0] == 'f')) {
+        int n = atoi(argv[2]);
+        if(argv[1][0] == 'o'){
+            printf("BITS\t\t\tORIGINAL PLUQ\t\t\tVECTORIZED PLUQ\t\t\tSPEEDUP\n\n");
+            for (long k = 0; k < 29; k++) {
+                double tt = 0.0;
+                long nb_iter = 0;
+                while (tt < 1.) {
+                    Matrix A = randomMatrix(n, n, primes[k]);
+                    int* P = NULL;
+                    int* Q = NULL;
+                    int rank = 0;
+                    clock_t start = clock();
+                    pluq_inplace(&A, &P, &Q, &rank, primes[k]);
+                    clock_t end = clock();
+                    tt += ((double)(end - start)) / CLOCKS_PER_SEC;
+                    
+                    nb_iter += 1;
+                }
+                tt = tt / nb_iter;
+                printf("%d\t\t\t%.12f\t\t\t",k + 2, tt);
+                double tt_original = tt;
+                tt = 0.0;
+                nb_iter = 0;
+                while (tt < 1.) {
+                    Matrix A = randomMatrix(n, n, primes[k]);
+                    int* P = NULL;
+                    int* Q = NULL;
+                    int rank = 0;
+                    clock_t start = clock();
+                    pluq_inplace_avx2(&A, &P, &Q, &rank, primes[k]);
+                    clock_t end = clock();
+                    tt += ((double)(end - start)) / CLOCKS_PER_SEC;
+                    
+                    nb_iter += 1;
             }
 
             tt = tt / nb_iter;
+            printf("%.12f\t\t\t%.4f\n\n", tt, tt_original/tt);
+            }
+        }else {
+            printf("BITS\t\t\tFLINT PLUQ\t\t\tVECTORIZED PLUQ\t\t\tSPEEDUP\n\n");
+                for (long k = 0; k < 29; k++) {
+                    double tt = 0.0;
+                    long nb_iter = 0;
+                    while (tt < 1.) {
+                        flint_rand_t state;
+                        fmpz_t p;
+                        fq_ctx_t ctx;
+                        fq_mat_t mat;
+                        slong rank;
+                        slong P[n];
+                        flint_randinit(state);
+                        flint_randseed(state, time(NULL), time(NULL));
+                        fmpz_init(p);
+                        fmpz_set_si(p, primes[k]);
+                        fq_ctx_init(ctx, p, 1, "x");
+                        fq_mat_init(mat, n, n, ctx);
+                        fq_mat_randtest(mat, state, ctx);
+                        clock_t start = clock();
+                        rank = fq_mat_lu(P , mat, rank, ctx);
+                        clock_t end = clock();
+                        tt += ((double)(end - start)) / CLOCKS_PER_SEC;
+
+                        fq_mat_clear(mat, ctx);
+                        fmpz_clear(p);
+                        flint_randclear(state);
+                        fq_ctx_clear(ctx);
+                        nb_iter += 1;
+                    }
+            tt = tt / nb_iter;
             printf("%d\t\t\t%.12f\t\t\t",k + 2, tt);
-            double tt_original = tt;
+            double tt_flint = tt;
             tt = 0.0;
             nb_iter = 0;
             while (tt < 1.) {
-                Matrix A = randomMatrix(200, 200, primes[k]);
+                Matrix A = randomMatrix(n, n, primes[k]);
                 int* P = NULL;
                 int* Q = NULL;
                 int rank = 0;
@@ -43,7 +106,8 @@ int main(int argc, char *argv[]) {
             }
 
             tt = tt / nb_iter;
-            printf("%.12f\t\t\t%.4f\n\n", tt, tt_original/tt);
+            printf("%.12f\t\t\t%.4f\n\n", tt, tt_flint/tt);
+            }
         }
         return 0;
     }
@@ -60,26 +124,25 @@ int main(int argc, char *argv[]) {
 
     int p = primes[bitlength-2];
 
-    // repeat computation several times if it is < 1 second
+
     double tt = 0.0;
     long nb_iter = 0;
     while (tt < 1.)
     {
-        // Create a matrix of specified dimensions
+        
         Matrix A = randomMatrix(m, n, p);
 
         int* P = NULL;
         int* Q = NULL;
-        Matrix LU;
         int rank = 0;
 
-        // Benchmark PLUQ computation
+        
         clock_t start = clock();
-        PLUQ(A, &P, &LU, &Q, &rank, p);
+        pluq_inplace_avx2(&A, &P, &Q, &rank, p);
         clock_t end = clock();
         tt += ((double)(end - start)) / CLOCKS_PER_SEC;
 
-        // Cleanup
+        
         free(A.data);
 
         nb_iter += 1;
